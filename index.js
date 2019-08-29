c124005752ad745fdd8751fc6a9ec5293a4d3251
Andriy Kashcha <anvaka@gmail.com>
2019-08-29 00:20:59 -0700

    added line strip

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export {default as Point} from './src/points/Point';
 
 export {default as LineCollection} from './src/lines/LineCollection';
 export {default as WireCollection} from './src/lines/WireCollection';
+export {default as LineStripCollection} from './src/lines/LineStripCollection';
 export {default as Element} from './src/Element';
 export {default as isWebGLEnabled} from './src/isWebGLEnabled';
 export {default as utils} from './src/glUtils';
