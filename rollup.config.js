b2ec7ab9d34c85d35dc8264c724f32803615bb12
Andriy Kashcha <anvaka@gmail.com>
2020-02-11 17:53:29 -0800

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
-const buble = require('rollup-plugin-buble');
-const cjs = require('rollup-plugin-commonjs');
-const node = require('rollup-plugin-node-resolve');
+import buble from '@rollup/plugin-buble';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 
 const version = process.env.VERSION || require('./package.json').version
 
 const banner =
   '/*!\n' +
   ' * wgl v' + version + '\n' +
-  ' * (c) 2017 Andrei Kashcha.\n' +
+  ' * (c) 2017-2020 Andrei Kashcha.\n' +
   ' * Released under the MIT License.\n' +
   ' */'
 export default {
   input: 'index.js',
   plugins: [
-		node(),
-		cjs(),
+		resolve(),
+		commonjs(),
 		buble()
 	],
-  sourcemap: true,
   output: [{
       format: 'umd',
       name: 'wgl',
-      file: 'build/wgl.js'
+      file: 'build/wgl.js',
+      sourcemap: true,
+      banner
     },
     {
       format: 'es',
       file: 'build/wgl.module.js'
     }
 	],
-	banner
 }
