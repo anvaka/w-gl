c15e615b1eda075994b0f3ca4851ec8b18a605e9
Andriy Kashcha <anvaka@gmail.com>
2020-03-07 08:14:30 -0800

    use shared build

--- examples/00. Simple Lines/index.js
+++ examples/00. Simple Lines/index.js
@@ -1,19 +1,19 @@
-import {createScene, WireCollection} from 'w-gl';
+const {createScene, WireCollection} = window.wgl;
 
 let scene = createScene(document.querySelector('canvas'));
 
 // let's draw a grid:
-let lines = new WireCollection(22);
-for (let row = 0; row <= 10; ++row) {
+let lines = new WireCollection(22,{width:4});
+for (let row = 0; row <= 100; ++row) {
   lines.add({
     from: {x: 0, y: row},
-    to: {x: 10, y: row}
+    to: {x: 100, y: row}
   });
 }
-for (let col = 0; col <= 10; ++col) {
+for (let col = 0; col <= 100; ++col) {
   lines.add({
     from: {x: col, y: 0},
-    to: {x: col, y: 10}
+    to: {x: col, y: 100}
   });
 }
 scene.appendChild(lines);
