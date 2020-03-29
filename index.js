3a2850e8dad0b9aec85db9d1d4796a9cc0be4bb9
Andriy Kashcha <anvaka@gmail.com>
2020-03-29 14:35:56 -0700

    dropped other navigations

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ export {default as Element} from './src/Element';
 export {default as isWebGLEnabled} from './src/isWebGLEnabled';
 export {default as utils} from './src/glUtils';
 export {default as toSVG} from './src/save/svg';
-export {default as createGameCamera} from './src/createGameCamera'
-export {default as createMapCamera} from './src/createMapCamera'
 export {default as createSpaceMapCamera} from './src/createSpaceMapCamera'
