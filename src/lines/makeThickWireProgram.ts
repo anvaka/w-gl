4185dadb64657567502b50c129dc2b3bb5e2e724
Andriy Kashcha <anvaka@gmail.com>
2020-05-02 13:01:25 -0700

    simplified line rendering

--- src/lines/makeThickWireProgram.ts
+++ src/lines/makeThickWireProgram.ts
@@ -41,13 +41,8 @@ export default function makeThickWireProgram(gl: WebGLRenderingContext, wireColl
 
   const positionBuffer = gl.createBuffer();
   const quadPositions = new Float32Array([
-    0, -0.5, 0,
-    0, -0.5, 1,
-    0, 0.5, 1,
-
-    0, -0.5, 0,
-    0, 0.5, 1,
-    0, 0.5, 0
+    -0.5, 0, -0.5, 1, 0.5, 1, // First 2D triangle of the quad
+    -0.5, 0, 0.5, 1, 0.5, 0   // Second 2D triangle of the quad
   ]);
 
   var api = {
@@ -92,8 +87,7 @@ export default function makeThickWireProgram(gl: WebGLRenderingContext, wireColl
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, quadPositions, gl.STATIC_DRAW);
     gl.enableVertexAttribArray(locations.attributes.aPosition);
-    gl.vertexAttribPointer(locations.attributes.aPosition, 3, gl.FLOAT, false, 0, 0)
-    gle.vertexAttribDivisorANGLE(locations.attributes.aPosition, 0);
+    gl.vertexAttribPointer(locations.attributes.aPosition, 2, gl.FLOAT, false, 0, 0)
 
 
     gl.bindBuffer(gl.ARRAY_BUFFER, lineBuffer);
@@ -101,25 +95,26 @@ export default function makeThickWireProgram(gl: WebGLRenderingContext, wireColl
 
     gl.enableVertexAttribArray(locations.attributes.aFrom)
     gl.vertexAttribPointer(locations.attributes.aFrom, lineSize, gl.FLOAT, false, lineStride, 0);
-    gle.vertexAttribDivisorANGLE(locations.attributes.aFrom, 1);
 
     if (allowColors) {
       gl.enableVertexAttribArray(locations.attributes.aFromColor);
       gl.vertexAttribPointer(locations.attributes.aFromColor, 4, gl.UNSIGNED_BYTE, true, lineStride, (lineSize) * 4);
-      gle.vertexAttribDivisorANGLE(locations.attributes.aFromColor, 1);
 
       gl.enableVertexAttribArray(locations.attributes.aTo)
       gl.vertexAttribPointer(locations.attributes.aTo, lineSize, gl.FLOAT, false, lineStride, (lineSize + 1) * 4)
-      gle.vertexAttribDivisorANGLE(locations.attributes.aTo, 1);
 
       gl.enableVertexAttribArray(locations.attributes.aToColor);
       gl.vertexAttribPointer(locations.attributes.aToColor, 4, gl.UNSIGNED_BYTE, true, lineStride, (2 * lineSize + 1) * 4);
-      gle.vertexAttribDivisorANGLE(locations.attributes.aToColor, 1);
     } else {
       gl.enableVertexAttribArray(locations.attributes.aTo)
       gl.vertexAttribPointer(locations.attributes.aTo, lineSize, gl.FLOAT, false, lineStride, lineSize * 4)
-      gle.vertexAttribDivisorANGLE(locations.attributes.aTo, 1);
     }
+
+    gle.vertexAttribDivisorANGLE(locations.attributes.aPosition, 0);
+    gle.vertexAttribDivisorANGLE(locations.attributes.aTo, 1);
+    gle.vertexAttribDivisorANGLE(locations.attributes.aFrom, 1);
+    gle.vertexAttribDivisorANGLE(locations.attributes.aFromColor, 1);
+    gle.vertexAttribDivisorANGLE(locations.attributes.aToColor, 1);
 
     // Now that everything is setup - render!
     gle.drawArraysInstancedANGLE(gl.TRIANGLES, 0, 6, wireCollection.count);
@@ -145,19 +140,32 @@ function getShadersCode(allowColors: boolean) {
     uniform vec2 uResolution;
   
   void main() {
+    // let's get the model point to clip space:
     vec4 clip0 = projectionMatrix * uView * uModel * vec4(aFrom, 1.0);
     vec4 clip1 = projectionMatrix * uView * uModel * vec4(aTo, 1.0);
+
+    // And from the clip space move to the screen pixels 
+    // (as we set width of the lines in pixels)
     vec2 screen0 = uResolution * (0.5 * clip0.xy/clip0.w + 0.5);
     vec2 screen1 = uResolution * (0.5 * clip1.xy/clip1.w + 0.5);
-    vec2 xBasis = normalize(screen1 - screen0);
-    vec2 yBasis = vec2(-xBasis.y, xBasis.x);
-    vec2 pt0 = screen0 + uWidth * (aPosition.x * xBasis + aPosition.y * yBasis);
-    vec2 pt1 = screen1 + uWidth * (aPosition.x * xBasis + aPosition.y * yBasis);
-    vec2 pt = mix(pt0, pt1, aPosition.z);
-    vec4 clip = mix(clip0, clip1, aPosition.z);
-    gl_Position = vec4(clip.w * (2.0 * pt/uResolution - 1.0), clip.z, clip.w);
 
-    vColor = ${allowColors ? 'mix(aFromColor.abgr, aToColor.abgr, aPosition.z)' : 'uColor'};
+    // this is direction along the x axis
+    vec2 xBasis = normalize(screen1 - screen0);
+    // But since we set the width, we get the direction along the Y:
+    vec2 yBasis = vec2(-xBasis.y, xBasis.x);
+
+    // Offset the original points:
+    vec2 pt0 = screen0 + uWidth * aPosition.x * yBasis;
+    vec2 pt1 = screen1 + uWidth * aPosition.x * yBasis;
+
+    // and interpolate along the Y of the instanced geometry 
+    // (remember, Y changes from 0 to 1):
+    vec2 pt = mix(pt0, pt1, aPosition.y);
+    vec4 clip = mix(clip0, clip1, aPosition.y);
+
+    // Finally move back to the clip space:
+    gl_Position = vec4(clip.w * (2.0 * pt/uResolution - 1.0), clip.z, clip.w);
+    vColor = ${allowColors ? 'mix(aFromColor.abgr, aToColor.abgr, aPosition.y)' : 'uColor'};
   }`,
     frag: `precision mediump float;
     varying vec4 vColor;
