ca03084e91187365bca5a4438767a9afea1e2345
Andriy Kashcha <anvaka@gmail.com>
2020-05-24 13:39:00 -0700

    export gl stuff

--- index.ts
+++ index.ts
@@ -8,5 +8,13 @@ export {default as Element} from './src/Element';
 export {default as isWebGLEnabled} from './src/isWebGLEnabled';
 export {default as utils} from './src/glUtils';
 export {default as toSVG} from './src/save/svg';
-export {default as createSpaceMapCamera} from './src/createSpaceMapCamera'
-export {default as createGuide} from './src/input/createGuide'
+export {default as createSpaceMapCamera} from './src/createSpaceMapCamera';
+export {default as createGuide} from './src/input/createGuide';
+
+// gl stuff:
+export {default as defineProgram} from './src/gl/defineProgram';
+export {default as ColorAttribute} from './src/gl/ColorAttribute';
+export {default as NumberAttribute} from './src/gl/NumberAttribute';
+export {default as FloatAttribute} from './src/gl/FloatAttribute';
+export {default as InstancedAttribute} from './src/gl/InstancedAttribute';
+export {default as GLCollection} from './src/GLCollection/GLCollection';
